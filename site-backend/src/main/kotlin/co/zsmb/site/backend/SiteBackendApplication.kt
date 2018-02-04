53bfc893cd51bc8d13256505f055b591a4ad93c5
zsmb13 <braunmarci@gmail.com>
2018-02-04 15:26:35 +0100

    Create multiplatform project modules

---
+++ site-backend/src/main/kotlin/co/zsmb/site/backend/SiteBackendApplication.kt
@@ -0,0 +1,11 @@
+package co.zsmb.site.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SiteBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<SiteBackendApplication>(*args)
+}
